33d07407404ba3ee3adf7d0194b75706b3656234
HarimEG <harimjgn@gmail.com>
2025-07-11 09:13:39 -0600

    Actualizar app.py

--- app.py
+++ app.py
@@ -119,15 +119,15 @@ with st.form("formulario"):
     st.markdown("---")
     st.subheader("Agregar Productos")
 
-col1, col2 = st.columns(2)
-with col1:
-    search_term = st.text_input("Buscar producto")
-    opciones_filtradas = productos_df[productos_df["Producto"].str.contains(search_term, case=False, na=False)]["Producto"].tolist()
-    producto = st.selectbox("Producto", opciones_filtradas if opciones_filtradas else ["Ningún resultado"])
-with col2:
-    ml = st.number_input("Mililitros", min_value=0.0, step=1.0)
+    col1, col2 = st.columns(2)
+    with col1:
+        search_term = st.text_input("Buscar producto")
+        opciones_filtradas = productos_df[productos_df["Producto"].str.contains(search_term, case=False, na=False)]["Producto"].tolist()
+        producto = st.selectbox("Producto", opciones_filtradas if opciones_filtradas else ["Ningún resultado"])
+    with col2:
+        ml = st.number_input("Mililitros", min_value=0.0, step=1.0)
 
-    agregar = st.form_submit_button("Agregar producto")
+        agregar = st.form_submit_button("Agregar producto")
 
     if "productos" not in st.session_state:
         st.session_state.productos = []
